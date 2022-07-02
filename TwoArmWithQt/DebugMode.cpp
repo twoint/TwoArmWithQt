757d08083995c2b806c6eae502a32c656239c996
chen <chenjian0218@hotmail.com>
2022-07-02 18:55:55 +0800

    能控制单个臂

--- TwoArmWithQt/DebugMode.cpp
+++ TwoArmWithQt/DebugMode.cpp
@@ -1,14 +1,17 @@
+#pragma execution_character_set("utf-8")//解决QLabel中文乱码
+
 #include "DebugMode.h"
 #include"ModeSelect.h"
-#include"ToolConnect.h"
-#include"RobotArm.h"
 #include"DebugPrintf.h"
+#include"RobotArm.h"
+#include"ToolConnect.h"
 
-extern ModeSelect* modeselect;
-extern ToolConnect ToolConnectCom;
 extern RobotArm Robot;
+extern ToolConnect ToolConnectCom;
 
-
+static float LeftValue = 1.0f;		//左臂示教比例
+static float RightValue = 1.0f;		//右臂示教比例
+float Angle_Dis[6] = { 0,0,0,0,0,0 };
 
 DebugMode::DebugMode(QWidget *parent)
 	: QWidget(parent)
@@ -16,39 +19,233 @@ DebugMode::DebugMode(QWidget *parent)
 	ui.setupUi(this);
 	//setWindowFlags(Qt::FramelessWindowHint | windowFlags());				//去窗口边框
 	//setAttribute(Qt::WA_TranslucentBackground);							//把窗口背景设置为透明
+	infoUpdateTimer = new QTimer(this);										//传感器信息刷新定时器
+	infoUpdateTimer->start(33);
 
 	connect(ui.btn_exit, SIGNAL(clicked()), this, SLOT(on_btn_exit()));
-	connect(ui.btn_lead_gripper_on, SIGNAL(clicked()), this, SLOT(on_btn_lead_gripper_on()));
-	connect(ui.btn_lead_gripper_off, SIGNAL(clicked()), this, SLOT(on_btn_lead_gripper_off()));
-	connect(ui.btn_arrester_grab_on, SIGNAL(clicked()), this, SLOT(on_btn_arrester_grab_on()));
-	connect(ui.btn_arrester_grab_off, SIGNAL(clicked()), this, SLOT(on_btn_arrester_grab_off()));
+	connect(infoUpdateTimer, SIGNAL(timeout()), this, SLOT(infoUpdateFrame()));//import frame when timeout
+
+	//connect(ui.btn_lead_gripper_on, SIGNAL(clicked()), this, SLOT(on_btn_lead_gripper_on()));
+	//connect(ui.btn_lead_gripper_off, SIGNAL(clicked()), this, SLOT(on_btn_lead_gripper_off()));
+	//connect(ui.btn_arrester_grab_on, SIGNAL(clicked()), this, SLOT(on_btn_arrester_grab_on()));
+	//connect(ui.btn_arrester_grab_off, SIGNAL(clicked()), this, SLOT(on_btn_arrester_grab_off()));
+
+	connect(ui.btn_left_connect, SIGNAL(clicked()), this, SLOT(on_btn_left_connect()));
+	connect(ui.btn_right_connect, SIGNAL(clicked()), this, SLOT(on_btn_right_connect()));
+
+	connect(ui.btn_left_break_open, SIGNAL(clicked()), this, SLOT(on_btn_left_break_open()));
+	connect(ui.btn_left_break_close, SIGNAL(clicked()), this, SLOT(on_btn_left_break_close()));
+	connect(ui.btn_right_break_open, SIGNAL(clicked()), this, SLOT(on_btn_right_break_open()));
+	connect(ui.btn_right_break_close, SIGNAL(clicked()), this, SLOT(on_btn_right_break_close()));
+
+	connect(ui.btn_left_arm_forword, SIGNAL(clicked()), this, SLOT(on_btn_left_arm_forward()));
+	connect(ui.btn_left_arm_backward, SIGNAL(clicked()), this, SLOT(on_btn_left_arm_backward()));
+	connect(ui.btn_left_arm_left, SIGNAL(clicked()), this, SLOT(on_btn_left_arm_left()));
+	connect(ui.btn_left_arm_right, SIGNAL(clicked()), this, SLOT(on_btn_left_arm_right()));
+	connect(ui.btn_left_arm_up, SIGNAL(clicked()), this, SLOT(on_btn_left_arm_up()));
+	connect(ui.btn_left_arm_down, SIGNAL(clicked()), this, SLOT(on_btn_left_arm_down()));
+
+	//connect(ui.btn_right_arm_forword, SIGNAL(clicked()), this, SLOT(on_btn_right_arm_forward()));
+	//connect(ui.btn_right_arm_backward, SIGNAL(clicked()), this, SLOT(on_btn_right_arm_backward()));
+	//connect(ui.btn_right_arm_left, SIGNAL(clicked()), this, SLOT(on_btn_right_arm_left()));
+	//connect(ui.btn_right_arm_right, SIGNAL(clicked()), this, SLOT(on_btn_right_arm_right()));
+	//connect(ui.btn_right_arm_up, SIGNAL(clicked()), this, SLOT(on_btn_right_arm_up()));
+	//connect(ui.btn_right_arm_down, SIGNAL(clicked()), this, SLOT(on_btn_right_arm_down()));
+
 
 }
 
+void DebugMode::infoUpdateFrame()
+{
+	//左臂连接信息
+	if (!Robot.LeftInf.Connect)
+	{
+		ui.label_left_arm_connect->setText("未连接");
+		ui.label_left_arm_connect->setStyleSheet("QLabel{background-color:rgb(255,0,0);border-radius:10px;color:rgb(255,255,255);}");
+	}
+	else
+	{
+		ui.label_left_arm_connect->setText("已连接");
+		ui.label_left_arm_connect->setStyleSheet("QLabel{background-color:rgb(0,255,0);border-radius:10px;color:rgb(255,255,255);}");
+	}
+
+	//右臂连接信息
+	if (!Robot.RightInf.Connect)
+	{
+		ui.label_right_arm_connect->setText("未连接");
+		ui.label_right_arm_connect->setStyleSheet("QLabel{background-color:rgb(255,0,0);border-radius:10px;color:rgb(255,255,255);}");
+	}
+	else
+	{
+		ui.label_right_arm_connect->setText("已连接");
+		ui.label_right_arm_connect->setStyleSheet("QLabel{background-color:rgb(0,255,0);border-radius:10px;color:rgb(255,255,255);}");
+	}
+
+	//左臂抱闸信息
+	if (!Robot.LeftInf.Brake)
+	{
+		ui.label_left_arm_break->setText("抱闸关闭");
+		ui.label_left_arm_break->setStyleSheet("QLabel{background-color:rgb(255,0,0);border-radius:10px;color:rgb(255,255,255);}");
+	}
+	else
+	{
+		ui.label_left_arm_break->setText("抱闸开启");
+		ui.label_left_arm_break->setStyleSheet("QLabel{background-color:rgb(0,255,0);border-radius:10px;color:rgb(255,255,255);}");
+
+	}
+
+	//右臂抱闸信息
+	if (!Robot.RightInf.Brake)
+	{
+		ui.label_right_arm_break->setText("抱闸关闭");
+		ui.label_right_arm_break->setStyleSheet("QLabel{background-color:rgb(255,0,0);border-radius:10px;color:rgb(255,255,255);}");
+
+	}
+	else
+	{
+		ui.label_right_arm_break->setText("抱闸开启");
+		ui.label_right_arm_break->setStyleSheet("QLabel{background-color:rgb(0,255,0);border-radius:10px;color:rgb(255,255,255);}");
+	}
+
+	//右臂角度信息
+	//ui.L_Joint_1->setValue(CurURData.Left_q_actual[1] * 180 / pi);
+}
+
+/************************机械臂网络连接、抱闸设置******************************/
+void DebugMode::on_btn_left_connect()
+{
+	Robot.CreatSocket("192.168.1.101", 8010);
+}
+
+void DebugMode::on_btn_right_connect()
+{
+	Robot.OpenCom("COM1", 115200, 0, 8, 1);//连接网络
+}
+
+void DebugMode::on_btn_left_break_open()
+{
+	Robot.Set(LeftArm, OpenBrake);
+}
+
+void DebugMode::on_btn_left_break_close()
+{
+	Robot.Set(LeftArm, CloseBrake);
+}
+
+void DebugMode::on_btn_right_break_open()
+{
+	Robot.Set(RightArm, OpenBrake);
+}
+
+void DebugMode::on_btn_right_break_close()
+{
+	Robot.Set(RightArm, CloseBrake);
+}
+
+
+/************************工具控制******************************/
+//void DebugMode::on_btn_lead_gripper_on()
+//{
+//	ToolConnectCom.command(1, 1, 0, 0);
+//}
+//
+//void DebugMode::on_btn_lead_gripper_off()
+//{
+//	ToolConnectCom.command(1, 2, 0, 0);
+//}
+//
+//void DebugMode::on_btn_arrester_grab_on()
+//{
+//	ToolConnectCom.command(2, 1, 0, 0);
+//}
+//
+//void DebugMode::on_btn_arrester_grab_off()
+//{
+//	ToolConnectCom.command(2, 2, 0, 0);
+//}
+
+/************************方向控制******************************/
+void DebugMode::on_btn_left_arm_forward()
+{
+	Angle_Dis[1] = -LeftValue;
+	Robot.Set(LeftArm, PTPRel_TCP, Angle_Dis);
+}
+
+void DebugMode::on_btn_left_arm_backward()
+{
+	Angle_Dis[1] = LeftValue;
+	Robot.Set(LeftArm, PTPRel_TCP, Angle_Dis);
+}
+
+void DebugMode::on_btn_left_arm_left()
+{
+	Angle_Dis[0] = LeftValue;
+	Robot.Set(LeftArm, PTPRel_TCP, Angle_Dis);
+}
+
+void DebugMode::on_btn_left_arm_right()
+{
+	Angle_Dis[0] = -LeftValue;
+	Robot.Set(LeftArm, PTPRel_TCP, Angle_Dis);
+}
+
+void DebugMode::on_btn_left_arm_up()
+{
+	Angle_Dis[2] = LeftValue;
+	Robot.Set(LeftArm, PTPRel_TCP, Angle_Dis);
+}
+
+void DebugMode::on_btn_left_arm_down()
+{
+	Angle_Dis[2] = -LeftValue;
+	Robot.Set(LeftArm, PTPRel_TCP, Angle_Dis);
+}
+
+//void DebugMode::on_btn_right_arm_forward()
+//{
+//	float V[6] = { 0 };
+//	V[1] = -10 * RightValue;
+//	rightArm.MoveS(V);
+//}
+//
+//void DebugMode::on_btn_right_arm_backward()
+//{
+//	float V[6] = { 0 };
+//	V[1] = 10 * RightValue;
+//	rightArm.MoveS(V);
+//}
+//
+//void DebugMode::on_btn_right_arm_left()
+//{
+//	float V[6] = { 0 };
+//	V[0] = 10 * RightValue;
+//	rightArm.MoveS(V);
+//}
+//
+//void DebugMode::on_btn_right_arm_right()
+//{
+//	float V[6] = { 0 };
+//	V[0] = -10 * RightValue;
+//	rightArm.MoveS(V);
+//}
+//
+//void DebugMode::on_btn_right_arm_up()
+//{
+//	float V[6] = { 0 };
+//	V[2] = 10 * RightValue;
+//	rightArm.MoveS(V);
+//}
+//
+//void DebugMode::on_btn_right_arm_down()
+//{
+//	float V[6] = { 0 };
+//	V[2] = -10 * RightValue;
+//	rightArm.MoveS(V);
+//}
+
+
+/************************退出*************************/
 void DebugMode::on_btn_exit()
 {
 	emit jumpPageTo(1);
 }
-
-/************************工具控制******************************/
-void DebugMode::on_btn_lead_gripper_on()
-{
-	ToolConnectCom.command(1, 1, 0, 0);
-}
-
-void DebugMode::on_btn_lead_gripper_off()
-{
-	ToolConnectCom.command(1, 2, 0, 0);
-}
-
-void DebugMode::on_btn_arrester_grab_on()
-{
-	ToolConnectCom.command(2, 1, 0, 0);
-}
-
-void DebugMode::on_btn_arrester_grab_off()
-{
-	ToolConnectCom.command(2, 2, 0, 0);
-}
-
-
--- TwoArmWithQt/DebugMode.h
+++ TwoArmWithQt/DebugMode.h
@@ -1,8 +1,7 @@
 #pragma once
 #include <qwidget.h>
 #include"ui_DebugMode.h"
-
-
+#include <QTimer>
 
 class DebugMode :
 	public QWidget
@@ -15,20 +14,48 @@ public:
 
 private:
 	Ui::DebugModeClass ui;
-
+	QTimer *infoUpdateTimer;//传感器信息刷新
 
 signals:
 	void jumpPageTo(int n);
 
 private slots:
-	void on_btn_lead_gripper_on();//引线抓手开
-	void on_btn_lead_gripper_off();//引线抓手关
-	void on_btn_arrester_grab_on();//避雷器抓手开
-	void on_btn_arrester_grab_off();//避雷器抓手关
+
+	void infoUpdateFrame();
+
+	/*********************工具控制*************************/
+	//void on_btn_lead_gripper_on();//引线抓手开
+	//void on_btn_lead_gripper_off();//引线抓手关
+	//void on_btn_arrester_grab_on();//避雷器抓手开
+	//void on_btn_arrester_grab_off();//避雷器抓手关
+
+	/*********************方向控制*************************/
+	void on_btn_left_arm_forward();
+	void on_btn_left_arm_backward();
+	void on_btn_left_arm_left();
+	void on_btn_left_arm_right();
+	void on_btn_left_arm_up();
+	void on_btn_left_arm_down();
+
+	//void on_btn_right_arm_forward();
+	//void on_btn_right_arm_backward();
+	//void on_btn_right_arm_left();
+	//void on_btn_right_arm_right();
+	//void on_btn_right_arm_up();
+	//void on_btn_right_arm_down();
+
+	/*********************机械臂网络连接、抱闸设置*************************/
+	void on_btn_left_connect();
+	void on_btn_right_connect();
+	void on_btn_left_break_open();
+	void on_btn_left_break_close();
+	void on_btn_right_break_open();
+	void on_btn_right_break_close();
 
 
-
+	/*********************退出*************************/
 	void on_btn_exit();
+
 
 
 
